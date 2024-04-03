3c08c1b40b28cdae5914f1de58c71f5b25aed4a6
simonjuma_230 <simon.ombom22@students.dkut.ac.ke>
2024-04-03 23:31:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+//ARITHEMATIC OPERATORS IN JAVA SCRIPT
+const now = 2037 
+
+const ageJuma = now- 2024;
+const ageSerah = now -2023;
+
+console.log(ageJuma, ageSerah);
+console.log(ageJuma * 2, ageSerah /2);
+
+console.log(ageJuma ** 2, ageSerah ** 2);// Exponetial  operator
+ 
+
+//String concentation using the + operator
+let firstName ='Simon';
+let secondName = 'Juma';
+
+console.log(firstName + secondName);
+
+//ASSIGNMENT OPERATOR
+let x = 10 + 5;
+x += 10;  //x = x + 10 
+x *= 4; // x = x * 4
+x++; //x = x + 1    
+x--;
+x--;
+console.log(x); 
+
+//COMPARISON OPERATORS
+console.log(ageJuma > ageSerah);
+
+let ageSimon = 18;
+let ageRose = 17;
+if (ageSimon > ageRose);
+  console.log("Simon is allowed to vote")
+  
+    console.log("Rose is too young to vote");
+  
+    console.log(ageSerah >= 18);
+    console.loog(ageJuma >= 18);
